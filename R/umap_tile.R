6a50305a619266a580e24fd11149da8b50a3fa8f
KYLE DOHERTY <kyledoherty@KYLEs-iMac-Pro.local>
2021-04-26 11:27:03 -0700

    specify raster namespace

--- R/umap_tile.R
+++ R/umap_tile.R
@@ -8,6 +8,6 @@ umap_tile <- function(tile_loc, out_dir, scl = TRUE, ...){
   umap_1 <- umap_2 <- tile[[1]]
   values(umap_1)[cells] <- uwot_out[,1]
   values(umap_2)[cells] <- uwot_out[,2]
-  umap_stack <- stack(umap_1, umap_2, tile)
-  writeRaster(umap_stack, filename = file.path(out_dir, basename(tile_loc)))
+  umap_stack <- raster::stack(umap_1, umap_2, tile)
+  raster::writeRaster(umap_stack, filename = file.path(out_dir, basename(tile_loc)))
 }
