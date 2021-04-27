d66dfe7370102c9dab636b3b44162a772905a7a8
KYLE DOHERTY <kyledoherty@for12.for.nau.edu>
2021-04-27 09:52:03 -0700

    refine rgb ras viz controls

--- R/p2t.R
+++ R/p2t.R
@@ -1,8 +1,10 @@
 p2t <- function(umap_dir, label_dir, ...) {
   umap_files <- list.files(umap_dir, full.names = TRUE)
+  band_count <- raster::nlayers(raster::stack(umap_files[1]))
+  band_choices <- as.numeric(seq_len(band_count))
   
   ui <- shiny::fillPage(
-    leaflet::leafletOutput('rgb_leaf', height = '100%', width = '100%'),
+    leaflet::leafletOutput('rgb_leaf', height = '75%', width = '100%'),
     shiny::absolutePanel(
       shiny::selectInput(
         inputId = 'img_sel',
@@ -11,7 +13,41 @@ p2t <- function(umap_dir, label_dir, ...) {
       ),
       top = 10,
       left = 10
-    )
+    ),
+    shiny::absolutePanel(
+      shiny::sliderInput(inputId = 'img_qt',
+                         label = 'Image quantiles',
+                         ticks = FALSE,
+                         value = c(0.01,0.99),
+                         min = 0,
+                         max = 1,
+                         step = 0.01),
+      top = 75,
+      left = 10,
+      width = 150
+    ), 
+    
+    shiny::absolutePanel(shiny::selectInput(
+      inputId = 'b_1',
+      label = 'R band',
+      choices = band_choices,
+      selected = 3
+    ),
+    shiny::selectInput(
+      inputId = 'b_2',
+      label = 'G band',
+      choices = band_choices,
+      selected = 4
+    ),
+    shiny::selectInput(
+      inputId = 'b_3',
+      label = 'B band',
+      choices = band_choices,
+      selected = 5
+    ),
+    top = 155,
+    left = 10,
+    width = 60)
   )
   
   server <- function(input, output) {
@@ -22,12 +58,25 @@ p2t <- function(umap_dir, label_dir, ...) {
     })
     
     rgb_ras <- shiny::reactive({
-      raster::stack(fname())[[3:5]]
+      base_st <- raster::stack(fname())
+      b1 <- base_st[[as.numeric(input$b_1)]]
+      b2 <- base_st[[as.numeric(input$b_2)]]
+      b3 <- base_st[[as.numeric(input$b_3)]]
+      raster::stack(b1,b2,b3)
     })
     
-    output$rgb_plot <- shiny::renderPlot(raster::plotRGB(rgb_ras()))
+    ras_qts <- shiny::reactive({
+      c(input$img_qt[1], input$img_qt[2])
+    })
+    
     output$rgb_leaf <- leaflet::renderLeaflet(leaflet::leaflet(options = leaflet::leafletOptions(zoomControl = FALSE)) %>%
-                                                leafem::addRasterRGB(rgb_ras(), ...))
+                                                leafem::addRasterRGB(rgb_ras(), 
+                                                                     r = 1, 
+                                                                     g = 2,
+                                                                     b = 3,
+                                                                     quantiles = ras_qts(), 
+                                                                     max
+                                                                       ))
     
   }
   
